d324876ec9e2bb7da0ae4329fc4d3268a190f6a0
blankcx330 <2486119856@qq.com>
2025-11-01 22:14:03 +0800

    学习react

--- 创建和嵌套组件/src/App.jsx
+++ 创建和嵌套组件/src/App.jsx
@@ -1,12 +1,16 @@
+import React from 'react'
+import './App.css'
+
 function Hello() {
   return (
-    <h1>Hello World</h1>
+    <h1 className='hello' >Hello World</h1>
   );
 }
 
 const Button = () => <button>Click Me</button>
 
-
+// App()通常会被挂载到 HTML 页面的根元素（如 <div id="root">）上
+// 成为所有其他组件的父级容器。
 function App() {
   return (
     <>
