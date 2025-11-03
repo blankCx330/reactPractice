b059bb33197e6e22343e890dea810adb9d7e60a1
blankcx330 <2486119856@qq.com>
2025-11-03 23:06:56 +0800

    学习react

--- 创建和嵌套组件/src/App.jsx
+++ 创建和嵌套组件/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 // react中使用下面的方式定义一个组件
@@ -14,7 +14,7 @@ function Hello() {
 
 const Button = () => <button>Click Me</button>
 
-
+// 上面组件使用的语法被称为JSX
 // react 组件可以返回多个元素，需要使用一个父元素包裹
 // JSX 比 HTML 更加严格。你必须闭合标签
 // 如 <br />。你的组件也不能返回多个 JSX 标签。
@@ -31,10 +31,54 @@ function Page() {
 
 // 在 JSX 中可以使用变量
 const text = "这是一个文本变量"
+const pObj = {
+  title: "这是一个属性变量标题",
+  content: "这是一个属性变量内容"
+}
+const addNum = (a, b) => a + b;
+const arr = [1, 2, 3, 4, 5];
+const style = {
+  backgroundColor: '#000',
+  width: '100px',
+  height: '100px',
+}
 
+// 在 JSX 中可以使用表达式
+// JSX 中使用 {}时
+// 相当于告诉 React：“这里需要解析一段 JavaScript 表达式”
 function Text() {
   return (
-    <div>{text}</div>
+    <>
+      <div>{text}</div>
+      <h2>{pObj.title}</h2>
+      <p>{pObj.content}</p>
+      <div>{'这是拼接字符'+text}</div>
+      <div>1 + 2 = {addNum(1,2)}</div>
+      <div>{arr.map((num, index) => (<p key={index} >{num}</p>))}</div>
+      
+      {/* style={ } 中必须要是一个对象 */}
+      {/* React 的内联样式系统基于 ​JavaScript 对象​ 实现
+          而不是传统的 CSS 字符串。 */}
+      <div style={style}></div>
+      <p></p>
+      <div style={{
+          backgroundColor: '#000',
+          width: '100px',
+          height: '100px',
+        }}></div>
+    </>
+  )
+}
+
+
+// 更新界面
+
+// 注意，useState()要在组件中声明
+// 修改数值需要调用解构的第二个函数
+function NumClick() {
+const [num, setNum] = useState(0);
+  return (
+    <button onClick={() => setNum(num=> num + 1)}>num:{num}</button>
   )
 }
 
@@ -48,6 +92,7 @@ function App() {
         <Button />
         <Page />
         <Text />
+        <NumClick />
       </div>
     </>
   )
