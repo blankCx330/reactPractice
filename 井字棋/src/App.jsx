7a3aaeac0979f318f466b33c5bf4aa8b23eb204e
blankcx330 <2486119856@qq.com>
2025-11-08 22:42:20 +0800

    学习react 井字棋游戏

--- 井字棋/src/App.jsx
+++ 井字棋/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 
-function Square() {
+const text = '';
+
+function Square({value}) {
   return (
     <>
-      <button className='square'></button>
+      <button className='square'>{ value }</button>
     </>
   )
 }
@@ -15,19 +17,19 @@ function App() {
     <>
       <div className='ground'>
         <div className='column-div'>
-          <Square />
-          <Square />
-          <Square />
+          <Square value='1' />
+          <Square value='2' />
+          <Square value='3' />
         </div>
         <div className='column-div'>
-          <Square />
-          <Square />
-          <Square />
+          <Square value='4' />
+          <Square value='5' />
+          <Square value='6' />
         </div>
         <div className='column-div'>
-          <Square />
-          <Square />
-          <Square />
+          <Square value='7' />
+          <Square value='8' />
+          <Square value='9' />
         </div>
       </div>   
     </>
