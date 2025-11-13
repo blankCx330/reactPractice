bf3eb61965d228eeec46e54f84209cdc512d4c1c
blankcx330 <2486119856@qq.com>
2025-11-13 22:49:08 +0800

    井字棋游戏 显示赢家

--- 井字棋/src/App.jsx
+++ 井字棋/src/App.jsx
@@ -8,7 +8,7 @@ function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || calcilateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -21,8 +21,42 @@ function Board() {
     setXIsNext(!xIsNext);
   }
 
+  const winner = calcilateWinner(squares);
+  // 这里的 status 不是状态变量
+  // 但status 的值依赖于 squares 和 xIsNext 这两个状态变量
+  // 当 squares xIsNext 这两个状态变量 更新导致组件重新渲染时
+  // status会重新计算
+  // 新的值会直接反映在 JSX 中
+  let status;
+  if(winner){
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'O' : 'X');
+  }
+
+  function calcilateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   return (
     <>
+    <p className='status'>{ status }</p>
       <div className='ground'>
         <div className='column-div'>
           {/* 
