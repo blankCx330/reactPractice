2d9859617220ff63ee4377f480aa9572814d327e
blankcx330 <2486119856@qq.com>
2026-03-18 00:39:34 +0800

    骨架组件添加脉冲动画animate-pulse

--- 天气咨询/src/components/skeleton/LeftContainerSkeleton.tsx
+++ 天气咨询/src/components/skeleton/LeftContainerSkeleton.tsx
@@ -3,8 +3,8 @@ import '../../css/LeftContainer.css'
 export default function LeftContainerSkeleton() {
   return (<>
   <div className="left-container">
-    <div className="today-weather"></div>
-    <div className="seven-day-weather"></div>
+    <div className="today-weather animate-pulse"></div>
+    <div className="seven-day-weather animate-pulse"></div>
   </div>
   </>)
 }
--- 天气咨询/src/components/skeleton/RightContainerSkeleton.tsx
+++ 天气咨询/src/components/skeleton/RightContainerSkeleton.tsx
@@ -6,18 +6,18 @@ export default function RightContainerSkeleton() {
     <div className="right-container">
       <div className="weather-data">
         <div className="weather-data-top">
-          <div className="wind-direction-and-air-data"></div>
-          <div className="sunrise-and-moonrise-times"></div>
+          <div className="wind-direction-and-air-data animate-pulse"></div>
+          <div className="sunrise-and-moonrise-times animate-pulse"></div>
         </div>
         <div className="weather-data-bottom">
-          <div className="weather-data-card"></div>
-          <div className="weather-data-card"></div>
-          <div className="weather-data-card"></div>
-          <div className="weather-data-card"></div>
+          <div className="weather-data-card animate-pulse"></div>
+          <div className="weather-data-card animate-pulse"></div>
+          <div className="weather-data-card animate-pulse"></div>
+          <div className="weather-data-card animate-pulse"></div>
         </div>
       </div>
       <div className="real-time-weather-data">
-        {list.map(list => <div className="real-time-card">{list}</div>)}
+        {list.map(list => <div className="real-time-card animate-pulse">{list}</div>)}
       </div>
     </div>
   </>)
